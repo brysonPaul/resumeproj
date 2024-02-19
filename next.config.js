83b514dd52574bf5c683c8338072c8fab57330af
Bryson Paul <89651994+brysonPaul@users.noreply.github.com>
2024-02-19 15:04:05 -0500

    added some images and pdfs

--- next.config.js
+++ next.config.js
@@ -5,12 +5,12 @@ const isProd = process.env.NODE_ENV === 'production';
 
 
 const nextConfig = {
-  //output: 'export',
-  // images: {
-  //   loader: 'akamai',
-  //   unoptimized: true,
-  //   path: '',
-  // },
+  output: 'export',
+  images: {
+    loader: 'akamai',
+    unoptimized: true,
+    path: '',
+  },
   assetPrefix: isProd ? '/resumeproj/' : '',
   basePath: isProd ? '/resumeproj' : '',
 };
