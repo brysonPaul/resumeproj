749c212e447b6f90f6183799e81efec958569787
Bryson Paul <89651994+brysonPaul@users.noreply.github.com>
2024-02-26 14:34:22 -0500

    added images to page

--- app/components/Project.js
+++ app/components/Project.js
@@ -1,12 +1,18 @@
 'use client'
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import "../styles.css";
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faLink } from '@fortawesome/free-solid-svg-icons';
 import { SimpleIconsDevpost } from './SimpleIconDevpost';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAppStoreIos } from '@fortawesome/free-brands-svg-icons';
 import { Chip } from "@nextui-org/react";
+import Image from "next/image";
+
+import useEmblaCarousel from 'embla-carousel-react';
+import Autoplay from "embla-carousel-autoplay";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 export default function Project(project) {
     project = project.project;
@@ -20,20 +26,39 @@ export default function Project(project) {
 
     }, [project]);
 
+    const [emblaRef, embla] = useEmblaCarousel({ loop: true }, [Autoplay()]);
 
+    const next = useCallback(() => {
+        if (!embla) return;
+        if (embla.canScrollNext()) {
+            embla.scrollNext();
+        } else {
+            embla.scrollTo(0);
+        }
+    }, [embla]);
+
+    const prev = useCallback(() => {
+        if (!embla) return;
+        if (embla.canScrollPrev()) {
+            embla.scrollPrev();
+        } else {
+            embla.scrollTo(emblaRef.length - 1);
+        }
+    }, [embla, emblaRef.length]);
 
     return (
+        //entire thing
         // Every project will have a name, description, date, and technologies
-        <div style={{ display: 'flex', height: '100%', flexDirection: 'column', borderColor: "white", borderTopWidth: '0.25px', borderBottomWidth: '0.25px', borderColor: 'white', padding: '1%', paddingLeft:'5%' }}>
+        <div style={{ display: 'flex', height: '100%', flexDirection: 'row', borderColor: "white", borderTopWidth: '0.25px', borderBottomWidth: '0.25px', borderColor: 'white', padding: '1%', paddingLeft: '5%' }}>
             {/* Entire side that doesnt have a photo */}
-            <div style={{ display: 'flex', flexDirection: 'column', height: '100%', width: '50%', gap: '5%' }}>
+            <div style={{ display: 'flex', flexDirection: 'column', height: '100%', width: '55%', gap: '5%' }}>
                 {/* title, links, and date */}
-                <div style={{ display: 'flex', flexDirection: 'row', justifyItems : 'space-between', alignItems: 'center', width: '100%', height:'10%', gap: '5%' }}>
-                        <p className='ptitle'>
-                            {project.name}
-                        </p>
+                <div style={{ display: 'flex', flexDirection: 'row', justifyItems: 'space-between', alignItems: 'center', width: '100%', height: '10%', gap: '5%' }}>
+                    <p className='ptitle'>
+                        {project.name}
+                    </p>
                     <div style={{ justifyItems: 'flex-start', display: 'flex', flexDirection: 'row', width: '50%', gap: '5%' }}>
-                        
+
 
                         {project.github && <a className="icon-links-projects flex flex-col" href={project.github}>
                             <FontAwesomeIcon icon={faGithub} title="View this Github Repo" />
@@ -61,32 +86,61 @@ export default function Project(project) {
                     </p>
                 </div>
 
-                {/* descriptions/ awards, */}
-                <div style={{ display: 'flex', flexDirection: 'column', height: '60%', padding: "1%" }}>
-                    <p style={{ marginTop: '4%', fontSize: '20px' }}>
-                        {project.description}
-                    </p>
-                    {project.awards && <p style={{ paddingTop: '4%', fontSize: '20px' }}>
-                        Awards: {project.awards}
-                    </p>}
+                <div style={{ display:'flex',flexDirection:'column', flexGrow: 1, height:'60%'}}>
+                    {/* descriptions/ awards, */}
+                    <div style={{ display: 'flex', flexDirection: 'column', height: '60%', padding: "1%" }}>
+                        <p style={{ marginTop: '4%', fontSize: '22px' }}>
+                            {project.description}
+                        </p>
+                        {project.awards && <p style={{ paddingTop: '4%', fontSize: '21px' }}>
+                            Awards: {project.awards}
+                        </p>}
 
-                </div>
-                {/* tech tags */}
-
-                {isLoading ? (
-                    // Display a loading indicator while waiting
-                    <div></div>
-                ) : (
-                    <div className="chip-container">
-                        <p style={{fontSize:'20px'}}>Tags:  </p>
-                        {chipArr.map((item, index) => (
-                            <Chip key={index} radius="md" size='lg'>{item}</Chip>
-                        ))}
                     </div>
-                )}
+                    {/* tech tags */}
+
+                    {isLoading ? (
+                        <div></div>
+                    ) : (
+                        <div className="chip-container">
+                            <p style={{ fontSize: '20px' }}>Tags:  </p>
+                            {chipArr.map((item, index) => (
+                                <Chip key={index} radius="md" size='lg'>{item}</Chip>
+                            ))}
+                        </div>
+                    )}
+                </div>
+
 
             </div>
 
+
+            {/* Images of the thing will go here */}
+            <div style={{ overflow: 'hidden', display: 'flex', justifyContent: 'flex-end', width: '50%' }} ref={emblaRef}>
+                <div className="embla__container_project">
+                    {project.images.map((item, index) => (
+                        <div key={index} className="embla__slide_project" >
+
+                            <button style={{ padding: "50px", width: '25%', height: '25%', fontSize: '40px', display: 'flex', alignItems: 'flex-end', justifyContent: 'flex-end' }} onClick={prev}>
+                                <FontAwesomeIcon icon={faArrowLeft}>
+                                </FontAwesomeIcon>
+                            </button>
+                            <Image
+                                src={item}
+                                alt={"Some photos of the project"}
+                                width={500}
+                                height={500}
+                            />
+                            <button style={{ padding: "50px", width: '25%', height: '25%', fontSize: '40px', display: 'flex', alignItems: 'flex-end', justifyContent: 'flex-start' }} onClick={next}>
+                                <FontAwesomeIcon icon={faArrowRight}>
+                                </FontAwesomeIcon>
+                            </button>
+                        </div>
+
+
+                    ))}
+                </div>
+            </div>
 
         </div>
 
--- app/projects/page.js
+++ app/projects/page.js
@@ -5,7 +5,7 @@ const projects = [
     {
         name: "Resume Website",
         date: "02/2024",
-        description: "Created a resume website for people interested to know a bit more about me and ways to contact! ( thanks again for visiting :) )",
+        description: "Created a responsive resume website for people interested to know a bit more about me and ways to contact ( thanks again for visiting )! ",
         technologies: "Javascript,React,NextJS,Tailwind CSS,NextUI",
         link: "https://brysonpaul.com/",
         images: ["/projects/about-me.jpg","/projects/keyboards.jpg"],
@@ -15,51 +15,51 @@ const projects = [
         name: "Project Mercury",
         date: "01/2023 - 12/2023",
         description: "Developed a cross-platform application in React Native for the United States Army Reserve,"
-            + " streamlining administrative tasks between leaders and soldiers to improve efficiency and decision-making within a unit.",
+            + " streamlining administrative tasks between leaders and soldiers to improve efficiency and decision-making within a unit. During my time I worked on 40+ critical bug fixes needed from the Reserves while refactoring 20k+ lines of code, and spearheaded the bolsering of RBAC and UI overhauls on the Web version",
         technologies: "Javascript,React-Native/React,AWS Lambda,PostgresSQL,Android Studio,Jira,Confluence",
         link: "https://www.ucf.edu/news/ucf-students-support-u-s-military-with-new-technology/",
         app: 'https://apps.apple.com/us/app/army-reserve-mercury/id1619569471',
-        images: ["/projects/pm-1.jpg","/projects/pm-2.jpg"],
+        images: ["/projects/pm-1.jpg","/projects/pm-2.png","/projects/pm-3.png"],
         
 
     },
     {
-        name: "Morgan and Morgan Mate",
+        name: "Morgan & Morgan Mate",
         date: "05/2023",
-        description: "Created an AI-powered tool that generates concise summaries of legal documents in multiple languages, saving time and resources for a given case",
+        description: "Created an AI-powered tool that generates concise summaries of legal documents with multi-lingual support, saving time and resources for a given case by allowing further understanding of legal documents without the need of a translator for non-native english speakers.",
         technologies: "Javascript,React-Native,Google Vision OCR,GPT-4 API",
         github: "https://github.com/maximusmith529/mogathan",
         devpost: 'https://devpost.com/software/morgan-morgan-mate',
-        images: ["/projects/mm-1","/projects/mm-2","/projects/mm-3"],
+        images: ["/projects/mm-1.jpg","/projects/mm-2.jpg","/projects/mm-3.jpg"],
     },
     {
         name: "UCF-GO",
         date: "03/2023 - 05/2023",
-        description: "Worked as a fullstack engineer on a Pokemon-Go esque game centered around catching monsters around my college campus",
+        description: "Worked as a fullstack engineer on a Pokemon-Go esque game centered around catching monsters around my college campus to learn a bit about each of the locations the monsters reside in!",
         technologies: "Javascript,React-Native/React,Google Maps API, MongoDB",
         github: 'https://github.com/Caitlin-Fabian/UCFGO',
         github_2 :'https://github.com/Caitlin-Fabian/LargeProject',
-        images: ["/projects/ucfg-1","/projects/ucfg-2","/projects/ucfg-3"],
+        images: ["/projects/ucf-go-1.PNG","/projects/ucf-go-2.PNG","/projects/ucf-go-3.PNG","/projects/ucf-go-4.PNG"],
     },
     {
         name: "Saberfy",
         date: "08/2022",
-        description: "Developed an CLI application during ShellHacks 2022 that analyzes music from Spotify and generates Beat Saber maps based on the beat, tempo and song structure",
+        description: "Developed an CLI application during ShellHacks 2022 that analyzes music from Spotify and generates Beat Saber maps based on the beat, tempo and song structure.",
         awards: "Second Place Shellhacks 2022, Best First Time Hackers",
         technologies: "Python,SpotDL,Spotify API",
         github: "https://github.com/maximusmith529/Saberfy",
         devpost: "https://devpost.com/software/saberfy",
-        images: ""
+        images: ["/projects/sfy-1.jpg","/projects/sfy-2.jpg"],
     }];
 
 export default function Projects() {
     return (
-        <main style={{ minHeight: "75vh",display: 'flex', flexDirection:'column',}}>
+        <main style={{ minHeight: "200vh",display: 'flex', flexDirection:'column',}}>
 
             <p className="title"> My Projects </p>
             <p style={{paddingLeft:'6%', paddingBottom:'2%', paddingTop:'2%', fontSize:'25px'}}>Take a look at my projects ive made in my time as a software engineer!</p>
 
-            <div  style={{display: 'flex', flexDirection:'column',height:'150vh'}}>
+            <div  style={{ flexDirection:'column',height:'100%'}}>
                 {projects.map((item,index) => (<Project key={index} project={projects[index]}/>))}
             </div>
         </main>
